999be10b02df4061cadefb666fce1d7f8664bd87
Romel Campbell <RomelCampbell@gmail.com>
2023-10-31 03:53:13 -0400

    Fibonacci sequence

--- Fibonacci.py
+++ Fibonacci.py
@@ -31,14 +31,16 @@ print(x)
 
 
 #### I dont understand where is my 3?
-a = 0
-b = a
-x = [ ]
+# In a Fibonacci sequence each number in the series 
+# is equal to the sum of the 2 preceding number on the sequence
 def fibonacci(n):
+    a = 0
+    b = 1
+    x = [ ]
     for i in range(0,n):
-     c = a + b
-     a = c
-     b = a
-     #c = str(c)
-     x.append(c)
-print(x)
+        c = a + b
+        a = b
+        b = c
+        x.append(c)
+        print(x)
+fibonacci(10)
---
+++ README.md
@@ -0,0 +1,6 @@
+# Notes
+
+## Fibonacci sequence
+
+- [Python Program to Print the Fibonacci sequence](https://www.geeksforgeeks.org/python-program-to-print-the-fibonacci-sequence/)
+- [Python playground](https://programiz.pro/ide/python)
