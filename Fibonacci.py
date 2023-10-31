80a113ec7721a796978f50ccc5520b2cd0d460be
Jabbar Campbell <Jabbar_Campbell@hotmail.com>
2023-10-31 11:11:38 -0400

    latest version for homework

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,5 +1,4 @@
 # This will dynamically make a list
-x=0
 x = [ ] # set up and empty list
 for i in range(0,6):
     k = str(i)
@@ -8,38 +7,30 @@ for i in range(0,6):
 print(x)
 
 
-# This will dynamically make a list
-x = 0
-a = 1
-b = 0
-n = 5
-x = [ ] # set up and empty list
-
-for i in range(0,n):
-    if x == 0:
-        c = a + b
-        a = c
-        b = a
-        x.append(c)
-    else:
-        x != 0
-        c = a + b
-        a = c
-        b = a
-        x.append(c)
-print(x)
 
 
-#### I dont understand where is my 3?
-a = 0
-b = 1
-n = 5
-x = []
+
 def fibonacci(n):
-    for i in range(0,n):
-     c = a + b
-     a = c
-     b = a
-     #c = str(c)
-     x.append(c)
-print(x)
+    #### I dont understand why
+    # a = b and not a = c
+    # b = c and not b = a
+    a = 0
+    b = 1
+    x = []
+    for i in range(0, n + 1):
+        if n == 0:
+            break
+        if x == []:
+            x.append(0)
+        if x != []:
+            c = a + b
+            a = b
+            b = c
+            x.append(c)
+        if i == 0:
+            x.append(1)
+            a = b
+            b = c
+        # c = str(c)
+    return(x)
+
